b614e4573b531cab0d8505053f8e56e38666fc7a
arashebuynow <arash.serej@ebuynow.com>
2022-12-06 16:35:12 +0330

    added modules to func

--- func.py
+++ func.py
@@ -1,5 +1,8 @@
+from mod1 import double_
+from mod2 import halve
+from mod3 import inc_
+
 if __name__ == "main::
     x = 10
-    y += x
+    y = double_(inc_(halve(x)))
     print(y)
-    print('Hello Parisan!')
